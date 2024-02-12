28eb2d9ef460c61a30bfdf217f59de7771967cf7
NachoGracia <gracianacho@gmail.com>
2024-02-12 16:03:06 +0100

    create conexion with mongo db & create the service

---
+++ src/utils/db.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+  try {
+    const db = await mongoose.connect(MONGO_URI);
+
+    const { name, host } = db.connection;
+
+    console.log(
+      `Conectada la DB 👌  en el host: ${host} con el nombre: ${name}`
+    );
+  } catch (error) {
+    console.log("No se ha conectado la db ❌", error.message);
+  }
+};
+
+module.exports = { connect };
