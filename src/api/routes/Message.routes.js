649a817042ea302030a1607f9dccfdbb10e691a9
clara <clarapalmero@email.com>
2024-02-13 11:13:29 +0100

    "messageRoutes & serviceRoutes"

---
+++ src/api/routes/Message.routes.js
@@ -0,0 +1,9 @@
+const { isAuth } = require("../../middleware/auth.middleware");
+/*const {
+  createMessage,
+} = require("../controllers/Message.controllers");*/
+
+const MessageRoutes = require("express").Router();
+
+MessageRoutes.post("/:idRecipient", [isAuth], createMessage);
+module.exports = MessageRoutes;
---
+++ src/api/routes/Service.routes.js
@@ -0,0 +1,7 @@
+const { upload } = require("../../middleware/files.middleware");
+/*const {
+  createService,
+} = require("../controllers/Service.controllers");*/
+const express = require("express");
+
+module.exports = ServiceRouteserviceRoutes;
