4db34c9123de0e458defe85025970a524edc0f9f
clara <clarapalmero@email.com>
2024-02-13 17:43:55 +0100

    Co-authored-by: Juliana Fernandez <julianafernandezlima@hotmail.com>

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use(express.urlencoded({ limit: "5mb", extended: false }));
 //const UserRoutes = require("./src/api/routes/User.routes");
 //app.use("/api/v1/users/", UserRoutes);
 
+const ServiceRoutes = require("./src/api/routes/Service.routes");
+app.use("/api/v1/services/", ServiceRoutes);
+
 app.use("*", (req, res, next) => {
   const error = new Error("Route not found");
   error.status = 404;
