b06e491eba99423dbdcef056a1958a23f7bf3435
JuFML <julianafernandezlima@hotmail.com>
2024-02-13 13:40:58 -0300

    create user general route - index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: false }));
 
-//const UserRoutes = require("./src/api/routes/User.routes");
-//app.use("/api/v1/users/", UserRoutes);
+const UserRoutes = require("./src/api/routes/User.routes");
+app.use("/api/v1/users/", UserRoutes);
 
 app.use("*", (req, res, next) => {
   const error = new Error("Route not found");
