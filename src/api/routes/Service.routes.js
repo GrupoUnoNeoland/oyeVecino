3f8176cdbe8a06298d428623c3ee9262408ce683
clara <clarapalmero@email.com>
2024-02-13 18:37:36 +0100

    index clara

--- src/api/routes/Service.routes.js
+++ src/api/routes/Service.routes.js
@@ -1,7 +1,22 @@
 const { upload } = require("../../middleware/files.middleware");
-/*const {
-  createService,
-} = require("../controllers/Service.controllers");*/
+const {
+  createServices,
+  deleteServices,
+  toggleUsers,
+  getByIdService,
+  getAllServices,
+  getByNameServices,
+  updateServices,
+} = require("../controllers/Service.controller");
 const express = require("express");
 
-module.exports = ServiceRouteserviceRoutes;
+const ServiceRoutes = express.Router();
+ServiceRoutes.post("/create", upload.array("images", 5), createServices);
+ServiceRoutes.delete("/delete/:id", deleteServices);
+ServiceRoutes.patch("/add/:id", toggleUsers);
+ServiceRoutes.get("/:id", getByIdService);
+ServiceRoutes.get("/", getAllServices);
+ServiceRoutes.get("/byName/:name", getByNameServices);
+ServiceRoutes.patch("/:id", upload.array("image", 5), updateServices);
+
+module.exports = ServiceRoutes;
