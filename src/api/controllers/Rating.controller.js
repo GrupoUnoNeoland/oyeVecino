d278dab6fe23c954d38a5e5cdd13c1d0367ca9b3
Natalia <naticastecno@gmail.com>
2024-02-16 11:32:18 +0100

    create rating controllers and rating model

---
+++ src/api/controllers/Rating.controller.js
@@ -0,0 +1,54 @@
+const User = require("../models/User.model");
+const Rating = require("../models/Rating.model");
+
+//------------------------------------* CREATE RATING--------------------------------------
+const createRating = async (req, res, next) => {
+  try {
+    await Rating.syncIndexes();
+
+    const customBody = {
+      stars: req.body?.stars,
+      userServiceTaker: req.user._id,
+      userServiceProvider: req.params.id,
+      companyServiceProvider: req.body?.company,
+    };
+
+    const newRating = new Rating(customBody);
+    const savedRating = await newRating.save();
+    if (savedRating) {
+      try {
+        await User.findByIdAndUpdate(req.params.id, {
+          $push: { starsReviews: savedRating._id },
+        });
+        try {
+          await User.findByIdAndUpdate(req.body.company, {
+            $push: { companyStarsReviews: savedRating._id },
+          });
+
+          return res.status(200).json(savedRating);
+        } catch (error) {
+          return res.status(404).json({
+            error: "Error catch al actualizar la empresa",
+            message: error.message,
+          });
+        }
+      } catch (error) {
+        return res.status(404).json({
+          error: "Error catch al actualizar el user",
+          message: error.message,
+        });
+      }
+    } else {
+      return res.status(404).json({
+        error: "Rating no guardado",
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({
+      error: "Error catch al crear el rating",
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { createRating };
---
+++ src/api/models/Rating.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const RatingSchema = new Schema(
+  {
+    stars: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+
+    userServiceProvider: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    companyServiceProvider: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Create Company model
+const Rating = mongoose.model("Rating", RatingSchema);
+module.exports = Rating;
