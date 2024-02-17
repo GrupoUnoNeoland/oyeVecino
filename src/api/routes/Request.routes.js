23e5bec3fa4c8be61027c7ab849bd3aab58331db
clara <clarapalmero@email.com>
2024-02-17 12:07:17 +0100

    "request routes"

--- src/api/routes/Request.routes.js
+++ src/api/routes/Request.routes.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const { upload } = require("../../middleware/files.middleware");
+
 const { isAuth } = require("../../middleware/auth.middleware");
 
-const {} = require("../controllers/Request.controller");
-
+const { createRequest } = require("../controllers/Request.controller");
 const express = require("express");
 
 const RequestRoutes = express.Router();
+
+RequestRoutes.post("/create", upload.array("images", 5), createRequest);
 
 module.exports = MessageRoutes;
