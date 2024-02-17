376c7828098eb993c68c2c975887be2e064445c5
clara <clarapalmero@email.com>
2024-02-17 11:19:09 +0100

    "city routes"

--- src/api/routes/City.routes.js
+++ src/api/routes/City.routes.js
@@ -1,13 +1,17 @@
-const CityRoutes = require("express").Router();
+const { upload } = require("../../middleware/files.middleware");
 const {
   createCity,
   deleteCity,
   getByIdCity,
   getAllCity,
 } = require("../controllers/City.controller");
-const { upload } = require("../../middleware/files.middleware");
+const express = require("express");
+
+const CityRoutes = express.Router();
 
 CityRoutes.post("/create", upload.array("images", 5), createCity);
-//CityRoutes.delete("/delete/:id", deleteCity);
-//CityRoutes.get("/:id", getByIdCity);
-//CityRoutes.get("/", getAllCity);
+CityRoutes.delete("/delete/:id", deleteCity);
+CityRoutes.get("/:id", getByIdCity);
+CityRoutes.get("/", getAllCity);
+
+module.exports = CityRoutes;
