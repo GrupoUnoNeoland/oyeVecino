7aa5e22d53a466b0bdd8e8824d49a0787b53b8f8
clara <clarapalmero@email.com>
2024-02-17 10:24:08 +0100

    "request routes"

---
+++ src/api/routes/Request.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { isAuth } = require("../../middleware/auth.middleware");
+
+const {} = require("../controllers/Request.controller");
+
+const express = require("express");
+
+const RequestRoutes = express.Router();
+
+module.exports = MessageRoutes;
