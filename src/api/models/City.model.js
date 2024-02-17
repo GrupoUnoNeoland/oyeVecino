623c0c0fa59160ae98fa6b6f63c62ffaaf0d9e7b
clara <clarapalmero@email.com>
2024-02-17 11:18:40 +0100

    "city model"

--- src/api/models/City.model.js
+++ src/api/models/City.model.js
@@ -7,6 +7,12 @@ const CitySchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
 
     neighborhoods: [
       { type: mongoose.Schema.Types.ObjectId, ref: "Neighborhood" },
