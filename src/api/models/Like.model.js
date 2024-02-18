875d84f01b2e9544667768b8a0af24bd933c3ca1
Natalia <naticastecno@gmail.com>
2024-02-18 22:27:58 +0100

    create like model

---
+++ src/api/models/Like.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const LikeSchema = new Schema(
+  {
+    like: {
+      type: Number,
+      required: true,
+      max: 1,
+    },
+    userLike: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: ["event", "statement"],
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+    },
+    statement: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Statement",
+    },
+  },
+  { timestamps: true }
+);
+
+const Like = mongoose.model("Like", LikeSchema);
+module.exports = Like;
