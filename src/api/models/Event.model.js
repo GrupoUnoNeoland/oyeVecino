051194181042195400f0f3e7d5b2e18cc3f8e2e4
Natalia <naticastecno@gmail.com>
2024-02-18 22:38:21 +0100

    update event model

--- src/api/models/Event.model.js
+++ src/api/models/Event.model.js
@@ -34,9 +34,9 @@ const EventSchema = new mongoose.Schema(
       type: Number,
       required: false,
     },
-    organizer: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    organizer: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Like" }],
     neighborhoods: [
       { type: mongoose.Schema.Types.ObjectId, ref: "Neighborhood" },
     ],
