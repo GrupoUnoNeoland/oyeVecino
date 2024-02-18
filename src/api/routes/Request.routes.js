ef418433789533aa0b4a79cff6ba9e5974ca03a5
clara <clarapalmero@email.com>
2024-02-18 18:28:28 +0100

    request files

--- src/api/routes/Request.routes.js
+++ src/api/routes/Request.routes.js
@@ -1,17 +1,31 @@
-const { isAuth } = require("../../middleware/auth.middleware");
-
+const { isAuth, isAuthAdmin } = require("../../middleware/auth.middleware");
+const { upload } = require("../../middleware/files.middleware");
 const {
   createRequest,
   deleteRequest,
   getByIdRequest,
   getAllRequest,
+  toggleUserInRequest,
+  toggleNeighborhoodInRequest,
 } = require("../controllers/Request.controller");
 const express = require("express");
 
 const RequestRoutes = express.Router();
 
-RequestRoutes.post("/create", createRequest);
-RequestRoutes.delete("/delete/:id", deleteRequest);
-RequestRoutes.get("/:id", getByIdRequest);
-RequestRoutes.get("/", getAllRequest);
+RequestRoutes.post(
+  "/create/",
+  [isAuth],
+  upload.single("document"),
+  createRequest
+);
+RequestRoutes.delete("/delete/:id", [isAuth], deleteRequest);
+RequestRoutes.get("/:id", [isAuthAdmin], getByIdRequest);
+RequestRoutes.get("/", [isAuth], getAllRequest);
+RequestRoutes.patch("/add/user/:id", [isAuth], toggleUserInRequest);
+RequestRoutes.patch(
+  "/add/neighborhood/:id",
+  [isAuth],
+  toggleNeighborhoodInRequest
+);
+
 module.exports = RequestRoutes;
