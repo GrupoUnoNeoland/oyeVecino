39b931a85a0323acbcbb0d743c9e09b8f288909e
Natalia <naticastecno@gmail.com>
2024-02-18 22:41:22 +0100

    getall services bystar route

--- src/api/routes/Service.routes.js
+++ src/api/routes/Service.routes.js
@@ -12,17 +12,29 @@ const {
   getByNameServices,
   updateServices,
   calculateStarsAverage,
+  getAllServicesStar,
 } = require("../controllers/Service.controller");
 const express = require("express");
 
 const ServiceRoutes = express.Router();
-ServiceRoutes.post("/create", [isAuth], upload.array("images", 5), createServices);
+ServiceRoutes.post(
+  "/create",
+  [isAuth],
+  upload.array("images", 5),
+  createServices
+);
 ServiceRoutes.delete("/delete/:id", [isAuth], deleteServices);
-ServiceRoutes.patch("/add/users/serviceoffered/:id", [isAuth], toggleUsersServiceOffered);
 ServiceRoutes.patch(
-  "/add/users/servicedemanded/:id", [isAuth],
+  "/add/users/serviceoffered/:id",
+  [isAuth],
+  toggleUsersServiceOffered
+);
+ServiceRoutes.patch(
+  "/add/users/servicedemanded/:id",
+  [isAuth],
   toggleUsersServiceDemanded
 );
+ServiceRoutes.get("/getallorder", [isAuth], getAllServicesStar);
 ServiceRoutes.patch("/add/neighborhoods/:id", [isAuth], toggleNeighborhoods);
 ServiceRoutes.patch("/add/comments/:id", [isAuth], toggleComments);
 ServiceRoutes.get("/:id", [isAuth], getByIdService);
@@ -30,7 +42,8 @@ ServiceRoutes.get("/", [isAuth], getAllServices);
 ServiceRoutes.get("/byName/:title", [isAuth], getByNameServices);
 ServiceRoutes.patch(
   "/update/service/:id",
-  upload.array("image", 5), [isAuth],
+  upload.array("image", 5),
+  [isAuth],
   updateServices
 );
 ServiceRoutes.post("/add/rating/:id", [isAuth], calculateStarsAverage);
