8d2704ac319a04cc4058204e9e2bfd7e6da207a6
Natalia <naticastecno@gmail.com>
2024-02-21 00:38:46 +0100

    Co-authored-by: Juliana Fernandez <julianafernandezlima@hotmail.com>

--- src/api/routes/Event.routes.js
+++ src/api/routes/Event.routes.js
@@ -13,17 +13,26 @@ const {
   toggleSponsor,
   updateEvent,
   toggleLike,
+  getAllEventsLike,
+  toggleCity,
 } = require("../controllers/Event.controller");
 
+EventRoutes.get("/getalllikes", [isAuth], getAllEventsLike);
 EventRoutes.get("/", [isAuth], getAllEvent);
 EventRoutes.delete("/delete/:id", [isAuth], deleteEvent);
 EventRoutes.get("/:id", [isAuth], getByIdEvent);
 EventRoutes.patch("/add/neighborhoods/:id", [isAuthAdmin], toggleNeighborhood);
 EventRoutes.patch("/add/comments/:id", [isAuth], toggleComment);
+EventRoutes.patch("/add/city/:id", [isAuth], toggleCity);
 EventRoutes.patch("/add/sponsors/:id", [isAuth], toggleSponsor);
 EventRoutes.patch("/add/likes/:id", [isAuth], toggleLike);
 
 EventRoutes.post("/create", upload.array("images", 5), [isAuth], createEvent);
-EventRoutes.patch("/update/event/:id", upload.array("image", 5), [isAuth], updateEvent);
+EventRoutes.patch(
+  "/update/event/:id",
+  upload.array("images", 5),
+  [isAuth],
+  updateEvent
+);
 
 module.exports = EventRoutes;
