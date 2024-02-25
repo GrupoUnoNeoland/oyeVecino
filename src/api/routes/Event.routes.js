9d9cab3fc60ee867c6ee2af50b3eb5e1173574f0
GrupoUnoNeoland <159558956+GrupoUnoNeoland@users.noreply.github.com>
2024-02-25 08:19:05 -0300

    Update Event.routes.js

--- src/api/routes/Event.routes.js
+++ src/api/routes/Event.routes.js
@@ -9,10 +9,8 @@ const {
   getAllEvent,
   getByIdEvent,
   toggleNeighborhood,
-  toggleComment,
   toggleSponsor,
   updateEvent,
-  toggleLike,
   getAllEventsLike,
   toggleCity,
   toggleOrganizer,
@@ -23,10 +21,8 @@ EventRoutes.get("/", [isAuth], getAllEvent);
 EventRoutes.delete("/delete/:id", [isAuth], deleteEvent);
 EventRoutes.get("/:id", [isAuth], getByIdEvent);
 EventRoutes.patch("/add/neighborhoods/:id", [isAuthAdmin], toggleNeighborhood);
-EventRoutes.patch("/add/comments/:id", [isAuth], toggleComment);
 EventRoutes.patch("/add/city/:id", [isAuth], toggleCity);
 EventRoutes.patch("/add/sponsors/:id", [isAuth], toggleSponsor);
-EventRoutes.patch("/add/likes/:id", [isAuth], toggleLike);
 EventRoutes.patch("/add/organizers/:id", [isAuth], toggleOrganizer);
 EventRoutes.post("/create", upload.array("images", 5), [isAuth], createEvent);
 EventRoutes.patch(
