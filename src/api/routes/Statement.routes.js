3e9986b0580b3b081543d07938be48bbab9a5f49
JuFML <julianafernandezlima@hotmail.com>
2024-03-09 12:57:32 -0300

    create tooggle like in statement

--- src/api/routes/Statement.routes.js
+++ src/api/routes/Statement.routes.js
@@ -13,12 +13,14 @@ const {
   updateStatement,
   getAllStatementLike,
   toggleCity,
+  toggleLike,
 } = require("../controllers/Statement.controller");
 
 StatementRoutes.get("/getalllike", [isAuth], getAllStatementLike);
 StatementRoutes.patch("/add/:id", [isAuth], toggleUser);
 StatementRoutes.patch("/add/neighborhoods/:id", [isAuth], toggleNeighborhood);
 StatementRoutes.patch("/add/city/:id", [isAuth], toggleCity);
+StatementRoutes.patch("/add/likes/:id", [isAuth], toggleLike);
 StatementRoutes.get("/", [isAuth], getAllStatement);
 StatementRoutes.delete("/delete/:id", [isAuth], deleteStatement);
 StatementRoutes.get("/:id", [isAuth], getByIdStatement);
